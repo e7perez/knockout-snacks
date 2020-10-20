54553393fb03c7f6e0933bd9b1d7bf51a74ef8d9
Emmanuel Perez <emmanueleperez@icloud.com>
2020-10-20 15:57:58 -0400

    thanks page

--- webform.php
+++ webform.php
@@ -30,3 +30,21 @@ if(isset($_POST['submit'])){
     }
 }
 ?>
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <title>Knockout Snacks -  Home of the Cosmic Pebbles</title>
+    <meta name="Knockout Snacks - Home of the Cosmic Pebbles" content="Knockout Snacks">
+    <meta name="Emmanuel Perez" content="Knockout Snacks">
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' >
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+<body>
+    <section id="thanks-page">
+        <div class="thanks-wrapper">
+            <h1>Thank you for contacting us. We'll be in touch shortly.</h1>
+        </div>
+    </section>
+</body>
